fc2bc1eab308b0e3276a31aa44ce16d3d4df6f6f
vipin.pahwa <vipin.pahwa@go-jek.com>
2021-07-25 23:38:54 +0530

    Initial Commit

---
+++ SettingsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by Vipin Pahwa on 25/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
