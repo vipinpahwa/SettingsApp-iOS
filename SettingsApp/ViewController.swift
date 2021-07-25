77f7132e553208109ffb196afd57a0983060c89e
vipin.pahwa <vipin.pahwa@go-jek.com>
2021-07-25 23:55:43 +0530

    Add models

--- SettingsApp/ViewController.swift
+++ SettingsApp/ViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColour: UIColor
+    let handler: (() -> Void)
+}
+
 class ViewController: UIViewController {
     
     private let tableView: UITableView = {
@@ -14,14 +21,24 @@ class ViewController: UIViewController {
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return table
     }()
+    
+    var models = [SettingsOption]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         title = "Settings"
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
+    }
+    
+    private func configure() {
+        self.models = Array(0...100).compactMap({ i in
+            SettingsOption(title: "Item \(i)", icon: UIImage(named: "house"), iconBackgroundColour: .systemPink) {
+            }
+        })
     }
 }
 
@@ -31,12 +48,13 @@ extension ViewController: UITableViewDelegate {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello world"
+        cell.textLabel?.text = model.title
         return cell
     }
 }
